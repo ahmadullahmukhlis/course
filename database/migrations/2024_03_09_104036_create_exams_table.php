7dc74e86590dc156ff88859e98b4adb55a5f39c5
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-03-09 15:12:59 +0430

    the exam migaration created

---
+++ database/migrations/2024_03_09_104036_create_exams_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\student;
+use App\Models\subject;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->integer('marks');
+            $table->foreignIdFor(student::class);
+            $table->foreignIdFor(subject::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};

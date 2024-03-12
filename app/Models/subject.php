9cabfeee5cc181d06b99692aa35374b3dd4f7363
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-03-12 17:00:28 +0430

    the problem fixed

--- app/Models/subject.php
+++ app/Models/subject.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class subject extends Model
 {
     use HasFactory;
+    public function student()
+    {
+        return $this->belongsToMany(student::class);
+    }
+    public function fees()
+    {
+        return $this->hasMany(fee::class);
+    }
+    public function exam()
+    {
+        return $this->HasMany(exam::class);
+    }
 }

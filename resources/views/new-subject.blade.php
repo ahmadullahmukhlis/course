f75665f5d6bfd29b4f56b6af191c514d10ad3b55
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-03-13 15:47:15 +0430

    the problem fixed

---
+++ resources/views/new-subject.blade.php
@@ -0,0 +1,48 @@
+@use('App\Models\subject', 'subject')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight d-flex justify-content-between">
+            <span>create student</span>
+
+        </h2>
+
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('students.store') }}" method="POST">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="row">
+                                <!-- Name Field -->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Name</label>
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                            id="name" name="name" placeholder="Ex: Mukhlis">
+                                        @error('name')
+                                            <span class="invalid-feedback"
+                                                role="alert"><strong>{{ $message }}</strong></span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary bg-purple-600">Save</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <x-link />
+
+</x-app-layout>
---
+++ resources/views/subject.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight d-flex justify-content-between">
+            <span>students</span>
+            <a href="{{ route('subjects.create') }}"class="btn btn-primary">create new student</a>
+        </h2>
+
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="table-responsive">
+                        <table class="table table-dark table-hover" id="student">
+                            <thead class="">
+                                <th>#</th>
+                                <th>name</th>
+                                <th>action</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($subjects as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->name }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    </div>
+    <x-link />
+</x-app-layout>
